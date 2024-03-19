e540097ff8988033b7349cbc1aea96894093098a
Ranny Naing <kxantnxaing@gmail.com>
2024-03-19 12:43:16 -0700

    heroku link fix

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ ckeditor = CKEditor(app)
 
 # MYSQL DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@localhost/db_name"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/our_users"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/our_users"
 
 # Heroku Postgressql
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://exxzdfhdskiddh:f75c935919f02f071192e38961ecd83368ec508a8533cd214ee37bd3680c50a1@ec2-44-220-7-157.compute-1.amazonaws.com:5432/d261tl97dqem1o"
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://capkiavrlzugwv:84174ea3b5ab7e014512e402a803c22b26ed6efdaee28957cc1e10ebadbf21a9@ec2-100-24-250-155.compute-1.amazonaws.com:5432/d3tq36bjv37m35"
 # Secret Key!!
 app.config['SECRET_KEY'] = "super_secret_key"
 
